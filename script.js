cbd383d7f9e760b9ea93dce40581e86be6b51a4b
Arpit Baheti <87475705+arpitbaheti2002@users.noreply.github.com>
2023-08-07 22:10:38 +0530

    removed twitter and setup form complete

--- script.js
+++ script.js
@@ -79,3 +79,35 @@ document.addEventListener('scroll', toggleFixedImageButton);
 
 // Call the toggle function once on page load to check if the button should be visible initially
 toggleFixedImageButton();
+
+
+// Form submission
+function submitForm(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Get form data
+  const form = document.getElementById('response-form');
+  const formData = new FormData(form);
+
+  // Add the current date to the form data
+  const currentDate = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
+  formData.append('Date', currentDate);
+
+  // Send form data using fetch
+  fetch('https://script.google.com/macros/s/AKfycby-G2IPLPZEy6nfpe0kn-jVZJSoFxHmqz49Z1FYDQXGmw8274asHvG1PNhVnHN7SH0vrg/exec', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    // Handle response if needed
+    if (response.ok) {
+      alert('Form submitted successfully.');
+    } else {
+      alert('Error submitting form.');
+    }
+  })
+  .catch(error => {
+    // Handle error
+    alert('Error submitting form: ' + error.message);
+  });
+}
